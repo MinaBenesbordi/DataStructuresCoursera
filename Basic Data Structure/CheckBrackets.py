13f626f26d15d131d7514649e77559b9d6061673
Beni <113256126+MinaBenesbordi@users.noreply.github.com>
2023-04-12 13:49:06 +0330

    Update CheckBrackets.py

--- Basic Data Structure/CheckBrackets.py
+++ Basic Data Structure/CheckBrackets.py
@@ -7,13 +7,13 @@ def check(open_brackets, close_brackets, str):
         if c in open_brackets:
             stack.append((c, i))
         elif c in close_brackets:
-            if not stack: #If the stack is empty, we don't have an opening bracket for our closing bracket, so we return the index of the first closing bracket
+            if not stack: #If the stack is empty, we don't have an opening bracket for our closing bracket, so we return the index of the first closing bracket.
                 return i
-            if open_brackets[stack[-1][0]] != c: #If the closing bracket doesn't mach the opening bracket, we return the first unmatched closing bracket
+            if open_brackets[stack[-1][0]] != c: #If the closing bracket doesn't match the opening bracket, we return the first unmatched closing bracket.
                 return i
             stack.pop()
     if stack:
-        return stack[-1][1]    #if stack doesn't have any closing bracket, we return the index of the first opening bracket
+        return stack[-1][1]    #If the stack doesn't have any closing bracket, we return the index of the first opening bracket.
     return "Success"
 
 str = input()
